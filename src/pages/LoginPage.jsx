562f3e7f0f795ad63729c3c2ae820994ac0137c2
Mehwash Nasir <asmanasreen388@gmail.com>
2026-01-07 01:29:50 +0530

    login layout

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,20 @@
+import Login from "../components/Auth/Login";
+
+export default function LoginPage({ switchToSignup }) {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-6 rounded w-80 shadow">
+        <h2 className="text-xl font-semibold mb-4 text-center">Login</h2>
+
+        <Login />
+
+        <p
+          className="text-sm text-blue-500 mt-4 cursor-pointer text-center"
+          onClick={switchToSignup}
+        >
+          Don't have an account? Signup
+        </p>
+      </div>
+    </div>
+  );
+}
