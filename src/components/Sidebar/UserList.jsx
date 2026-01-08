4cb5b2acf6bc83e46c4044a9783d4a2deea90e2d
Mehwash Nasir <asmanasreen388@gmail.com>
2026-01-08 19:19:01 +0530

    status

--- src/components/Sidebar/UserList.jsx
+++ src/components/Sidebar/UserList.jsx
@@ -1,27 +1,56 @@
+import { useEffect, useState } from "react";
+import { ref, onValue } from "firebase/database";
+import { db } from "../../firebase/firebase";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+
+
 const users = [
-  { id: 1, name: "Ayesha", online: true },
-  { id: 2, name: "Rahul", online: false },
-  { id: 3, name: "Zoya", online: true },
+  { id: "user1", name: "Ayesha" },
+  { id: "user2", name: "Rahul" },
+  { id: "user3", name: "Zoya" },
 ];
 
 export default function UserList() {
+  const [presence, setPresence] = useState({});
+
+  useEffect(() => {
+    const presenceRef = ref(db, "presence");
+
+    onValue(presenceRef, (snapshot) => {
+      setPresence(snapshot.val() || {});
+    });
+  }, []);
+
   return (
     <div className="w-64 bg-white border-r">
       <h2 className="p-4 font-semibold text-lg">Chats</h2>
 
-      {users.map((user) => (
-        <div
-          key={user.id}
-          className="px-4 py-3 flex justify-between hover:bg-gray-100 cursor-pointer"
-        >
-          <span>{user.name}</span>
-          <span
-            className={`h-2 w-2 rounded-full ${
-              user.online ? "bg-green-500" : "bg-gray-400"
-            }`}
-          />
-        </div>
-      ))}
+      {/* <button
+       onClick={() => signOut(auth)}
+        className="text-sm text-red-500 hover:underline"
+      >
+        Logout
+      </button> */}
+
+
+      {users.map((user) => {
+        const isOnline = presence[user.id]?.online;
+
+        return (
+          <div
+            key={user.id}
+            className="px-4 py-3 flex justify-between items-center hover:bg-gray-100"
+          >
+            <span>{user.name}</span>
+            <span
+              className={`h-2 w-2 rounded-full ${
+                isOnline ? "bg-green-500" : "bg-gray-400"
+              }`}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 }
