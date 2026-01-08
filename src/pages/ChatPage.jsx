7d5a7cdfb1345a9bffbd1d06cf09b305e83fd7cd
Mehwash Nasir <asmanasreen388@gmail.com>
2026-01-08 22:30:13 +0530

    update5

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,11 +1,16 @@
 import ChatWindow from "../components/Chat/ChatWindow";
 import UserList from "../components/Sidebar/UserList";
+import { useState } from "react";
+
+const [activeUser, setActiveUser] = useState(null);
+
 
 export default function ChatPage() {
   return (
     <div className="flex h-screen bg-gray-100">
-      <UserList />
-      <ChatWindow />
+      <UserList setActiveUser={setActiveUser} />
+      <ChatWindow activeUser={activeUser} />
+
     </div>
   );
 }
