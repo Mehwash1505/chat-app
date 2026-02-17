ed8980f9f31271f633d6a18a973ac13815c9eae5
Mehwash1505 <asmanasreen388@gmail.com>
2026-02-17 23:45:46 +0530

    Update UserList.jsx

--- src/components/Sidebar/UserList.jsx
+++ src/components/Sidebar/UserList.jsx
@@ -1,4 +1,4 @@
-// UserList.jsx
+// src/components/Sidebar/UserList.jsx
 import { useEffect, useState } from "react";
 import { ref, onValue } from "firebase/database";
 import { db } from "../../firebase/firebase";
