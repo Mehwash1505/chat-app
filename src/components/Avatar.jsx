f65181166f8d00b4cad0758a743553b2e928136f
Mehwash1505 <asmanasreen388@gmail.com>
2026-02-17 23:46:35 +0530

    Update Avatar.jsx

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,4 +1,4 @@
-// Avatar.jsx
+// src/components/Avatar.jsx
 export default function Avatar({ name }) {
   return (
     <div className="h-10 w-10 rounded-full bg-blue-500 text-white flex items-center justify-center">
